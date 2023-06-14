0c8278cc87a657c2ed6497572f147b04c681cd0d
Nabin khanal <khanalnabin310@gmail.com>
2023-06-14 20:21:22 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const endDate = " Jun 20 2023 23:00:00"
+document.getElementById("end-Date").innerText = endDate;
+const inputs=document.querySelectorAll("input")
+
+
+const clock = () => {
+  const end = new Date(endDate);
+  const now = new Date();
+  console.log(end)
+  console.log(now)
+ const diff=(end-now)/1000;
+ console.log(diff)
+ inputs[0].value=(Math.floor(diff/3600/24))
+ inputs[1].value=(Math.floor((diff/3600)%24))
+ inputs[2].value=(Math.floor((diff/60)%60))
+ inputs[3].value=(Math.floor((diff)%60))
+
+
+ 
+
+
+ 
+ //1 day=24hr,1hr=60min and 1 min=60sec
+};
+clock();
+
+setInterval(()=>{
+clock()
+},1000)
